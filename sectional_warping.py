b702e2396e80c5180242295f8bf185057af2d12d
Mirith <laurenshin15@gmail.com>
2025-12-11 15:22:08 -0800

    simplify logic for distribution of extra threads.  update output to be neater

--- sectional_warping.py
+++ sectional_warping.py
@@ -7,7 +7,7 @@
 """
 we should consider: 
 non-multiples of 2" wide warps 
-	extra should be evenly distributed on end sections 
+    extra should be evenly distributed on end sections 
 
 we need a minimum of 2" wide strips, they can be 2.1 or 2.2" etc. but not less 
 
@@ -28,10 +28,13 @@ import pdb
 from itertools import groupby
 """should be replaced by user inputs later"""
 
-number_sections = 8
+# for coastal linen tea towels
+#number_sections = 8
+#input_sequence = ["A8", "B48", "C6", "A30", "B6", "C18", "A6", "B78", "C6", "A48", "B6", "C31"] 
 
-
-input_sequence = ["A8", "B48", "C6", "A30", "B6", "C18", "A6", "B78", "C6", "A48", "B6", "C31"] 
+# for floral bouquet 
+number_sections = 9
+input_sequence = ["A547"]
 
 """"""
 
@@ -52,12 +55,12 @@ ends = 0
 
 # create total sequence 
 for stripe in input_sequence:
-	# pdb.set_trace()
-	ends = int(re.search(r'\d+', stripe).group(0))
-	color = re.search(r'[a-zA-Z]+', stripe).group(0)
-	total_ends += ends
-	for end in range(0,ends):
-		warp_color_sequence.append(color)
+    # pdb.set_trace()
+    ends = int(re.search(r'\d+', stripe).group(0))
+    color = re.search(r'[a-zA-Z]+', stripe).group(0)
+    total_ends += ends
+    for end in range(0,ends):
+        warp_color_sequence.append(color)
 
 # determine number of ends per section 
 approximate = total_ends // number_sections 
@@ -69,53 +72,28 @@ all_section_number_threads = [approximate]*number_sections
 
 # we will distribute the modulo, if any, across sections evenly, working from the outside in, alternating sides 
 if modulo != 0:
+    
+    left_index = 0
+    right_index = len(all_section_number_threads) - 1
 
-	# to get to an even number of remaining threads to distribute 
-	if modulo % 2 != 0:
-		all_section_number_threads[0] += 1 
-		modulo -= 1
-	
-	left_index = 0
-	right_index = len(all_section_number_threads) - 1
+    halfway = len(all_section_number_threads) / 2
+    while modulo > 0: 
+        all_section_number_threads[left_index] += 1
+        all_section_number_threads[right_index] += 1
+        left_index += 1
+        right_index -= 1 
+        modulo -= 2
 
-	# determine if even or odd number of sections 
+        # reset indexes if needed
+        if left_index > halfway:
+            left_index = 0
 
-	# even number of sections 
-	if (len(all_section_number_threads) % 2 == 0):
-		# determine true halfway point 
-		halfway = len(all_section_number_threads) / 2
-		while modulo > 0: 
-			all_section_number_threads[left_index] += 1
-			all_section_number_threads[right_index] += 1
-			left_index += 1
-			right_index -= 1 
-			modulo -= 2
+        if right_index < halfway:
+            right_index = len(all_section_number_threads)
 
-			# reset indexes if needed
-			if left_index > halfway:
-				left_index = 0
-
-			if right_index < halfway:
-				right_index = len(all_section_number_threads)
-
-
-	else:
-		# determine central index 
-		center = len(all_section_number_threads) // 2 
-		while modulo > 0: 
-			all_section_number_threads[left_index] += 1
-			all_section_number_threads[right_index] += 1
-			left_index += 1
-			right_index -= 1 
-			modulo -= 2
-
-			# reset indexes if needed
-			if left_index == center:
-				left_index = 0
-
-			if right_index == center:
-				right_index = len(all_section_number_threads)
-
+        if modulo == 1:
+            all_section_number_threads[left_index] += 1
+            modulo -= 1
 
 """now we are ready to slice warp sequence"""
 
@@ -130,18 +108,18 @@ selected_threads = []
 
 for section_length in all_section_number_threads:
 
-	# slice from the start of our section to the end of the section 
-	selected_threads = warp_color_sequence[section_start_index:section_start_index+section_length]
+    # slice from the start of our section to the end of the section 
+    selected_threads = warp_color_sequence[section_start_index:section_start_index+section_length]
 
-	# now group selected_threads properly 
-	grouped = groupby(selected_threads)
+    # now group selected_threads properly 
+    grouped = groupby(selected_threads)
 
-	for group in grouped: 
-		final_sections[current_section_number] += group[0] + " " + str(len(list(group[1]))) + " __ "  
-	# update everything for the next loop 
-	current_section_number += 1
-	section_start_index += section_length
+    for color, amount in grouped: 
+        final_sections[current_section_number] += color + ": " + str(len(list(amount))) + " \t "  
+    # update everything for the next loop 
+    current_section_number += 1
+    section_start_index += section_length
 
 
 for section in final_sections:
-	print(section[:-3])
+    print("Section: " + section[:-3])
